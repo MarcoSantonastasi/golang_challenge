fe5fbaaa556242f58f545007b50c4abe5f97bd8c
marcosantonastasi <marco.santonastasi@gmail.com>
2022-07-15 07:26:43 +0200

    done basic seeding

---
+++ .devcontainer/scripts/01-db-seed.sql
@@ -0,0 +1,17 @@
+INSERT INTO issuers (name)
+VALUES
+('issuer_A'),
+('issuer_B'),
+('issuer_C');
+
+INSERT INTO investors (name)
+VALUES
+('investors_A'),
+('investors_B'),
+('investors_C');
+
+INSERT INTO invoices (denom, amount,asking)
+VALUES
+('EUR', 15000, 10000),
+('EUR', 30000, 20000),
+('EUR', 45000, 30000);
