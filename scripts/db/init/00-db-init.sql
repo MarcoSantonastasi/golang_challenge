918130d9ed6d58f80115ca171f9865938d2f5fa5
Marco Santonastasi <marco.santonastasi@altesis-pro.com>
2022-08-04 11:51:38 +0000

    done bids VIEW

--- scripts/db/init/00-db-init.sql
+++ scripts/db/init/00-db-init.sql
@@ -81,6 +81,43 @@ COMMENT ON TABLE ledger
     IS 'Ledger records all tranfers of money,a nd matches it with a transaction for reconciliation';
 
 
+CREATE OR REPLACE VIEW bids (
+  transaction,
+  invoice,
+  issuer,
+  bidder_account
+)
+AS (
+  SELECT
+    tx.id as transaction,
+    tx.invoice as invoice,
+    inv.issuer as issuer,
+    bid_up.debit as bidder_account,
+    coalesce(bid_up.amount, 0::bigint) - coalesce(bid_down.amount, 0::bigint) as amount
+  FROM transactions as tx
+  JOIN invoices as inv ON tx.invoice = inv.id
+  JOIN (SELECT id FROM accounts WHERE type = 'ESCROW'::type_account_type FETCH FIRST ROW ONLY) as escrow ON true
+  LEFT JOIN LATERAL (
+    SELECT
+      transaction,
+      debit,
+      sum(amount) as amount
+    FROM ledger l
+    WHERE l.credit = escrow.id
+    GROUP BY transaction, debit
+    ) AS bid_up ON bid_up.transaction = tx.id
+  LEFT JOIN LATERAL (
+    SELECT
+      transaction,
+      credit,
+      sum(amount)as amount
+    FROM ledger l
+    WHERE l.debit = escrow.id
+    GROUP BY transaction, credit
+    ) AS bid_down ON bid_down.transaction = tx.id
+);
+
+
 CREATE OR REPLACE FUNCTION update_account_balance_on_ledger_insert() 
 RETURNS TRIGGER AS $$
 BEGIN
