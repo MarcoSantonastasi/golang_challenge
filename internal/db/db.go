8f230a37899be64a7398c6507d04369b82c5f414
Marco Santonastasi <marco.santonastasi@altesis-pro.com>
2022-08-04 17:15:04 +0000

    made connection to DB parametric

--- internal/db/db.go
+++ internal/db/db.go
@@ -39,20 +39,30 @@ func (db *Db) GetAllInvoices() []*pb.Invoice {
 	return nil
 }
 
-var DockerPG = Db{}
+func NewPGDB(
+	pg_user string,
+	pg_pwd string,
+	pg_hostname string,
+	pg_dbname string,
+) *Db {
+	newPgDb := Db{}
 
-func init() {
-	//
-	// POSTGRES_USER=postgres
-	// POSTGRES_PASSWORD=postgres
-	// POSTGRES_DB=postgres
-	// POSTGRES_HOSTNAME=localhost
-	//
-
-	conn, err := pg.Connect(context.Background(), fmt.Sprintf("postgres://%s:%s@%s:5432/%s", os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_HOSTNAME"), os.Getenv("POSTGRES_DB")))
+	conn, err := pg.Connect(context.Background(), fmt.Sprintf("postgres://%s:%s@%s:5432/%s", pg_user, pg_pwd, pg_hostname, pg_dbname))
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
 	}
-	DockerPG.Conn = conn
+
+	newPgDb.Conn = conn
+
+	return &newPgDb
 }
+
+//
+// POSTGRES_USER=postgres
+// POSTGRES_PASSWORD=postgres
+// POSTGRES_DB=postgres
+// POSTGRES_HOSTNAME=localhost
+//
+
+var DockerPG = NewPGDB(os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"), os.Getenv("POSTGRES_HOSTNAME"), os.Getenv("POSTGRES_DB"))
