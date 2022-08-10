35ee0ef98a405831f679845ec1844c37775e81b5
marcosantonastasi <marco.santonastasi@gmail.com>
2022-08-10 18:12:27 +0000

    doen simple e2e tests for lists

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -1,58 +1,66 @@
 package e2etest
 
 import (
+	"context"
 	"encoding/json"
-	"flag"
+	"fmt"
 	"io"
 	"log"
+	"net"
 	"os"
 	"path"
 	"reflect"
 	"runtime"
 	"testing"
+	"time"
 
 	pb "github.com/marcosantonastasi/arex_challenge/api/arex/v1"
 	client "github.com/marcosantonastasi/arex_challenge/internal/client"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+	db "github.com/marcosantonastasi/arex_challenge/internal/db"
+	repos "github.com/marcosantonastasi/arex_challenge/internal/repos"
+	server "github.com/marcosantonastasi/arex_challenge/internal/server"
+	grpc "google.golang.org/grpc"
+	insecure "google.golang.org/grpc/credentials/insecure"
 )
 
-var AllInvestorsList []*pb.Investor
-var AllIssuersList []*pb.Issuer
-var AllInvoicesList []*pb.Invoice
+var expectData = struct {
+	allInvestorsList []*pb.Investor
+	allIssuersList   []*pb.Issuer
+	allInvoicesList  []*pb.Invoice
+}{}
 
-var InvestorServiceClient client.InvestorServiceClient
-var IssuerServiceClient client.IssuerServiceClient
-var InvoiceServiceClient client.InvoiceServiceClient
-
-var (
-	addr       = flag.String("addr", "localhost:50051", "the address to connect to")
-	globalConn *grpc.ClientConn
-)
+var clientServices = struct {
+	investor client.InvestorServiceClient
+	issuer   client.IssuerServiceClient
+	invoice  client.InvoiceServiceClient
+}{}
 
 func TestE2E_GetAllInvestors(t *testing.T) {
 	testCases := []struct {
 		desc    string
-		client  client.InvestorServiceClient
+		client  pb.InvestorServiceClient
 		want    pb.GetAllInvestorsResponse
 		wantErr bool
 	}{
 		{
 			desc:    "queries the db for Investors",
-			client:  InvestorServiceClient,
-			want:    pb.GetAllInvestorsResponse{Data: AllInvestorsList},
+			client:  clientServices.investor,
+			want:    pb.GetAllInvestorsResponse{Data: expectData.allInvestorsList},
 			wantErr: false,
 		},
 	}
 	for _, tt := range testCases {
 		t.Run(tt.desc, func(t *testing.T) {
-			got, err := tt.client.GetAllInvestors()
+			log.Printf("running %s", tt.desc)
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+			defer cancel()
+			got, err := tt.client.GetAllInvestors(ctx, &pb.Empty{})
 			if (err != nil) != tt.wantErr {
-				t.Errorf("Got InvestorServiceServer.GetAllInvestors() error = %v, instead expected error %v", err, tt.wantErr)
+				t.Errorf("Got GetAllInvestors() error = %v, instead expected error %v", err, tt.wantErr)
 				return
 			}
 			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Got InvestorServiceServer.GetAllInvestors() = %v, but wanted %v", got, tt.want)
+				t.Errorf("Got GetAllInvestors() = %v, but wanted %v", got, tt.want)
 			}
 
 		})
@@ -62,26 +70,29 @@ func TestE2E_GetAllInvestors(t *testing.T) {
 func TestE2E_GetAllIssuers(t *testing.T) {
 	testCases := []struct {
 		desc    string
-		client  client.IssuerServiceClient
+		client  pb.IssuerServiceClient
 		want    pb.GetAllIssuersResponse
 		wantErr bool
 	}{
 		{
 			desc:    "queries the db for Issuers",
-			client:  IssuerServiceClient,
-			want:    pb.GetAllIssuersResponse{Data: AllIssuersList},
+			client:  clientServices.issuer,
+			want:    pb.GetAllIssuersResponse{Data: expectData.allIssuersList},
 			wantErr: false,
 		},
 	}
 	for _, tt := range testCases {
 		t.Run(tt.desc, func(t *testing.T) {
-			got, err := tt.client.GetAllIssuers()
+			log.Printf("running %s", tt.desc)
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+			defer cancel()
+			got, err := tt.client.GetAllIssuers(ctx, &pb.Empty{})
 			if (err != nil) != tt.wantErr {
-				t.Errorf("Got IssuerServiceServer.GetAllIssuers() error = %v, instead expected error %v", err, tt.wantErr)
+				t.Errorf("Got GetAllIssuers() error = %v, instead expected error %v", err, tt.wantErr)
 				return
 			}
 			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Got IssuersServiceServer.GetAllIssuers() = %v, but wanted %v", got, tt.want)
+				t.Errorf("Got GetAllIssuers() = %v, but wanted %v", got, tt.want)
 			}
 
 		})
@@ -91,26 +102,29 @@ func TestE2E_GetAllIssuers(t *testing.T) {
 func TestE2E_GetAllInvoices(t *testing.T) {
 	testCases := []struct {
 		desc    string
-		client  client.InvoiceServiceClient
+		client  pb.InvoiceServiceClient
 		want    pb.GetAllInvoicesResponse
 		wantErr bool
 	}{
 		{
 			desc:    "queries the db for Invoices",
-			client:  InvoiceServiceClient,
-			want:    pb.GetAllInvoicesResponse{Data: AllInvoicesList},
+			client:  clientServices.invoice,
+			want:    pb.GetAllInvoicesResponse{Data: expectData.allInvoicesList},
 			wantErr: false,
 		},
 	}
 	for _, tt := range testCases {
 		t.Run(tt.desc, func(t *testing.T) {
-			got, err := tt.client.GetAllInvoices()
+			log.Printf("running %s", tt.desc)
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+			defer cancel()
+			got, err := tt.client.GetAllInvoices(ctx, &pb.Empty{})
 			if (err != nil) != tt.wantErr {
-				t.Errorf("Got InvoicersServiceServer.GetAllInvoices() error = %v, instead expected error %v", err, tt.wantErr)
+				t.Errorf("Got GetAllInvoices() error = %v, instead expected error %v", err, tt.wantErr)
 				return
 			}
 			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("Got InvoicesServiceServer.GetAllInvoices() = %v, but wanted %v", got, tt.want)
+				t.Errorf("Got GetAllInvoices() = %v, but wanted %v", got, tt.want)
 			}
 
 		})
@@ -118,66 +132,111 @@ func TestE2E_GetAllInvoices(t *testing.T) {
 }
 
 func TestMain(m *testing.M) {
+
+	log.Printf("TestMain()")
+
+	loadTestData()
+
+	s := startBufferedServer()
+	defer s.Stop()
+
+	conn := startBufferedClient()
+	defer conn.Close()
+	
+	os.Exit(m.Run())
+}
+
+func loadTestData() {
+	log.Printf("loadTestData()")
+	
 	_, b, _, _ := runtime.Caller(0)
 	path := path.Dir(b)
-
-	investorsFile, investorsFileErr := os.Open(path + "/../../data/investors.json")
+	
+	investorsFile, investorsFileErr := os.Open(path + "/../data/investors.json")
 	if investorsFileErr != nil {
-		panic("cannot open " + path + "/../../data/investors.json")
+		panic("cannot open " + path + "/../data/investors.json")
 	}
 	investorsData, investorsDataErr := io.ReadAll(investorsFile)
 	if investorsDataErr != nil {
-		panic("cannot read " + path + "/../../data/investors.json")
+		panic("cannot read " + path + "/../data/investors.json")
 	}
-	investorsJsonErr := json.Unmarshal(investorsData, &AllInvestorsList)
+	investorsJsonErr := json.Unmarshal(investorsData, &expectData.allInvestorsList)
 	if investorsJsonErr != nil {
-		panic("cannot parse (unmarshall) JSON data from " + path + "/../../data/investors.json")
+		panic("cannot parse (unmarshall) JSON data from " + path + "/../data/investors.json")
 	}
 	defer investorsFile.Close()
-
-	issuersFile, issuersFileErr := os.Open(path + "/../../data/issuers.json")
+	
+	issuersFile, issuersFileErr := os.Open(path + "/../data/issuers.json")
 	if issuersFileErr != nil {
-		panic("cannot open " + path + "/../../data/issuers.json")
+		panic("cannot open " + path + "/../data/issuers.json")
 	}
 	issuersData, issuersDataErr := io.ReadAll(issuersFile)
 	if issuersDataErr != nil {
-		panic("cannot read " + path + "/../../data/issuers.json")
+		panic("cannot read " + path + "/../data/issuers.json")
 	}
-	issuersJsonErr := json.Unmarshal(issuersData, &AllIssuersList)
+	issuersJsonErr := json.Unmarshal(issuersData, &expectData.allIssuersList)
 	if issuersJsonErr != nil {
-		panic("cannot parse (unmarshall) JSON data form " + path + "/../../data/issuers.json")
+		panic("cannot parse (unmarshall) JSON data form " + path + "/../data/issuers.json")
 	}
 	defer issuersFile.Close()
-
-	invoicesFile, invoicesFileErr := os.Open(path + "/../../data/invoices.json")
+	
+	invoicesFile, invoicesFileErr := os.Open(path + "/../data/invoices.json")
 	if invoicesFileErr != nil {
-		panic("cannot open " + path + "/../../data/invoices.json")
+		panic("cannot open " + path + "/../data/invoices.json")
 	}
 	invoicesData, invoicesDataErr := io.ReadAll(invoicesFile)
 	if invoicesDataErr != nil {
-		panic("cannot read " + path + "/../../data/invoices.json")
+		panic("cannot read " + path + "/../data/invoices.json")
 	}
-	invoicesJsonErr := json.Unmarshal(invoicesData, &AllInvoicesList)
+	invoicesJsonErr := json.Unmarshal(invoicesData, &expectData.allInvoicesList)
 	if invoicesJsonErr != nil {
-		panic("cannot parse (unmarshall) JSON data from " + path + "/../../data/invoices.json")
+		panic("cannot parse (unmarshall) JSON data from " + path + "/../data/invoices.json")
 	}
 	defer invoicesFile.Close()
+	
+}
 
-	flag.Parse()
+func startBufferedServer() (s *grpc.Server) {
+	
+	log.Printf("startBufferedServer()")
+	
+	const port int = 50051
+	
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s = grpc.NewServer()
+	
+	pb.RegisterInvestorServiceServer(s, &server.InvestorServiceServer{Repo: &repos.InvestorsRepository{Db: &db.Db{Conn: db.DockerPG.Conn}}})
+	pb.RegisterIssuerServiceServer(s, &server.IssuerServiceServer{Repo: &repos.IssuersRepository{Db: &db.Db{Conn: db.DockerPG.Conn}}})
+	pb.RegisterInvoiceServiceServer(s, &server.InvoiceServiceServer{Repo: &repos.InvoicesRepository{Db: &db.Db{Conn: db.DockerPG.Conn}}})
+	log.Printf("server listening at %v", lis.Addr())
+	go func() {
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+		s.Stop()
+	}
+	}()
+	
+	return s
+}
 
-	conn, err := grpc.Dial(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+func startBufferedClient() (conn *grpc.ClientConn) {
+	
+	log.Printf("startBufferedClient()")
+	addr := "localhost:50051"
+	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("could not not connect to the gRPC server: %v", err)
+		conn.Close()
+		return nil
 	}
 
-	globalConn = conn
-	defer conn.Close()
+	clientServices.investor = client.NewInvestorServiceClient(conn)
+	clientServices.issuer = client.NewIssuerServiceClient(conn)
+	clientServices.invoice = client.NewInvoiceServiceClient(conn)
 
-	InvestorServiceClient = client.NewInvestorServiceClient(conn)
-	InvoiceServiceClient = client.NewInvoiceServiceClient(conn)
-	IssuerServiceClient = client.NewIssuerServiceClient(conn)
+	return conn
 
-	code := m.Run()
-
-	os.Exit(code)
 }
