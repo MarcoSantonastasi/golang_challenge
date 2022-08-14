713a65afa7f2ebfd53f0ffee1cd427bd9e155c02
marcosantonastasi <marco.santonastasi@gmail.com>
2022-08-14 08:36:52 +0000

    redefine return of db procedure bid

--- scripts/db/init/00-db-init.sql
+++ scripts/db/init/00-db-init.sql
@@ -189,41 +189,37 @@ BEFORE INSERT ON bids
 
 
 CREATE OR REPLACE FUNCTION bid (
+  OUT _bid_id bigint,
   INOUT _invoice_id uuid,
   INOUT _bidder_account_id uuid,
   INOUT _offer bigint
 )
 AS $$
+
 DECLARE
-  _escrow_account_id uuid;
-  _invoice_state type_invoice_state;
-  _bid_id bigint;
+	_escrow_account_id uuid;
+	_invoice_state type_invoice_state;
 
 BEGIN
-  SELECT id FROM accounts
-  WHERE
-    type = 'ESCROW'::type_account_type
-  FETCH FIRST ROW ONLY
-  INTO _escrow_account_id;
+	SELECT id FROM accounts
+	WHERE
+		type = 'ESCROW'::type_account_type FETCH FIRST ROW ONLY INTO _escrow_account_id;
 
-  INSERT INTO bids (
-    invoice_id,
-    bidder_account_id,
-    offer
-  )
-  VALUES (
-    _invoice_id,
-    _bidder_account_id,
-    _offer
-  )
-  RETURNING id INTO _bid_id;
-
-  INSERT INTO transactions(bid_id, credit_account_id, debit_account_id, amount)
-  VALUES (_bid_id, _escrow_account_id, _bidder_account_id, _offer);
-
+	INSERT INTO bids (invoice_id, bidder_account_id, offer)
+		VALUES(_invoice_id, _bidder_account_id, _offer)
+	RETURNING
+		id, invoice_id, bidder_account_id, offer
+  INTO
+    _bid_id, _invoice_id, _bidder_account_id, _offer;
+	
+  INSERT INTO transactions (bid_id, credit_account_id, debit_account_id, amount)
+		VALUES(_bid_id, _escrow_account_id, _bidder_account_id, _offer);
+	
   RETURN;
 END;
-$$ LANGUAGE plpgsql;
+$$
+LANGUAGE plpgsql;
+
 
 
 
